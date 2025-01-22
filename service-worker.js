4642c0b253941bdc7d9128d4888a6a9b9124ec96
soyoung <soyoung@soyoungui-MacBookAir.local>
2025-01-22 17:07:56 +0900

    change the web push receive function

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,17 @@
 //웹 푸시 수신
-self.addEventListener('push', event => {
-    const data = event.data.json();
-    self.registration.showNotification('web push!', {
-        body: Text,
-        data: {
-            url: 'https://github.com/syounng'
-        }
-    });
-});
+// self.addEventListener('push', event => {
+//     const data = event.data.json();
+//     self.registration.showNotification('web push!', {
+//         body: Text,
+//         data: {
+//             url: 'https://github.com/syounng'
+//         }
+//     });
+// });
+
+self.addEventListener('push', () => {
+    self.ServiceWorkerRegistration.sendNotification('you got the web push', {})
+})
 
 //웹 푸시 알림 클릭
 self.addEventListener('notificationclick', function (event) {
