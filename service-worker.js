cdb530b2cfad3fd0de70ad718be09d1eb3e890c7
syounng <stellaj0522@gmail.com>
2025-01-22 18:33:54 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,4 @@
 //웹 푸시 수신
-// self.addEventListener('push', event => {
-//     const data = event.data.json();
-//     self.registration.showNotification('web push!', {
-//         body: Text,
-//         data: {
-//             url: 'https://github.com/syounng'
-//         }
-//     });
-// });
-
-// self.addEventListener('push', () => {
-//     self.ServiceWorkerRegistration.sendNotification('you got the web push', {})
-// })
-
 self.addEventListener('push', function (e) {
     let data = {};
 
